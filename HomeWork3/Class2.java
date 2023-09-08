06d1a6f19d3e0c393d5395accc9ec5ab39e88a02
R-Roman-E <115876121+R-Roman-E@users.noreply.github.com>
2023-09-08 14:36:58 +0300

    Update Class2.java

--- HomeWork3/Class2.java
+++ HomeWork3/Class2.java
@@ -1,1 +1,17 @@
+package HW_3;
 
+public class class_2 implements MyIf{
+    public int getCat(){
+        return 2012;
+    }
+
+    public int getOwner(){
+    return 1989;
+}
+
+    @Override
+    public int getUserID() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUserID'");
+    }
+}
