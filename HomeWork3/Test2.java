1e2fd09573e5ee4c787f7fc363c5ea9e6115e4b8
R-Roman-E <115876121+R-Roman-E@users.noreply.github.com>
2023-09-08 14:37:38 +0300

    Update Test2.java

--- HomeWork3/Test2.java
+++ HomeWork3/Test2.java
@@ -1,1 +1,31 @@
+package HW_3;
 
+public class test2 implements test1{
+    int age;
+   
+
+    public test2(){
+      age = 11;
+       
+    }
+    public int getNext(){
+     age = 11;
+
+       return age;
+     
+    }
+
+    public void reset(){
+
+     age = 10;
+
+    }
+
+    public void setNum(int x){
+   
+       age = x;
+       
+    }
+
+    
+}
