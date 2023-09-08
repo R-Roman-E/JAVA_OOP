b785a788ab921eb238b8aa032ca44d8131934ef8
R-Roman-E <115876121+R-Roman-E@users.noreply.github.com>
2023-09-08 14:30:44 +0300

    Update Class.java

--- HomeWork3/Class.java
+++ HomeWork3/Class.java
@@ -1,1 +1,18 @@
+package HW_3;
 
+public class MyClass implements Method2{
+    @Override
+    public void met1() {
+        System.out.println("Кот - Мэйсон");
+    }
+
+    @Override
+    public void met2() {
+       System.out.println("Возраст - 11");
+    }
+
+    @Override
+    public void met3() {
+         System.out.println("Владелец - Дарья");
+    }
+}
