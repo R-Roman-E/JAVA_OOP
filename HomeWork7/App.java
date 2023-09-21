e64de24fbda5647e7c8182939f18635d22239126
R-Roman-E <115876121+R-Roman-E@users.noreply.github.com>
2023-09-21 14:55:08 +0300

    Update App.java

--- HomeWork7/App.java
+++ HomeWork7/App.java
@@ -1,1 +1,7 @@
-
+ublic class App {
+    public static void main(String[] args) {
+        ComplexNumberView view = new ComplexNumberView();
+        ComplexNumberController controller = new ComplexNumberController(view);
+        controller.execute();
+    }
+}
